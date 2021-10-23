a99cc406e311e01022047466fb2f9c60983ed07d
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-10-23 13:37:37 +0300

    Add files via upload

--- coffee.js
+++ coffee.js
@@ -1,24 +1,35 @@
+let number = prompt("Сколько напитков вы хотите заказать?");
+for (let i = 0; i < number; i++) {
+    var question = prompt("Какой напиток вы хотите заказать?")
+    const myOrder = order(question);
+    alert(myOrder)
+}
+// let summa = str.substr(str.length - 2);
+// let num = Number(summa);
+// let obsum = num + num;
+// alert(obsum);
 function order(drink) {
     let str = "Ваш заказ: ";
     let sum = "Сумма: ";
     switch (drink) {
         case "эспрессо":
-            str += "эспрессо" + '</br>' + sum + "30грн";
+            str += "эспрессо" + '</br>' + sum + 30;
             break;
         case "капучино":
-            str += "капучино" + '</br>' + sum + "40грн";
+            str += "капучино" + '</br>' + sum + 40;
             break;
         case "латте":
-            str += "латте" + '</br>' + sum + "45грн";
+            str += "латте" + '</br>' + sum + 45;
             break;
         case "горячий шоколад":
-            str += "горячий шоколад" + '</br>' + sum + "70грн";
+            str += "горячий шоколад" + '</br>' + sum + 70;
             break;
         default:
             break;
     }
+    let summa = str.substr(str.length - 2);
+    let num = Number(summa);
+    let obsum = num + num;
+    alert(obsum);
     return str;
 }
-
-const myOrder = order("эспрессо");
-alert(myOrder);
